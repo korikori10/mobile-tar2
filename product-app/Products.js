87c7adb6a50b664cd2a08694dacd44c5e5f99178
Tom <tolas22@gmail.com>
2017-11-26 14:25:55 +0200

    backbutton
    added a back button

--- product-app/Products.js
+++ product-app/Products.js
@@ -31,14 +31,17 @@ $(document).on('vclick', '#DynamicList li a', function () {
     //}
 });
 
-
 function renderProducts(results) {
     //this is the callBackFunc 
     results = $.parseJSON(results.d);
+   // imageFullPath = ;
     $.each(results, function (i, row) {
-        dynamicLi = '<li> <a href="" data-id="' + row.Id + '"> <h3>' + row.Name + '</h3><span class="ui-li-count"> ' + row.ProductAmount+ '</span></a></li>'
-        $('#DynamicList').append(dynamicLi);
-        $('#DynamicList').listview('refresh');
+       console.log(JSON.stringify(row));
+       if (row.ImagePath == null) { imageFullPath = "/images/no-img.jpg" }
+            else { imageFullPath = row.ImagePath }
+        dynamicList = '<li> <img src="'+ imageFullPath + '"/> <a href="" data-id="' + row.Id + '">  <h3>' + row.Title + '</h3><p>Price: ' + row.Price + '</br>Inventory: '+ row.Inventory + '</p></a></li>'
+        $('#DynamicListproducts').append(dynamicList);
+        $('#DynamicListproducts').listview('refresh');
     })
 }
 
