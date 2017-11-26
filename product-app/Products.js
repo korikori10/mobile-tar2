a9b7760640ac67e05d79ab4bcec490f3e84b2e09
Tom <tolas22@gmail.com>
2017-11-26 16:33:48 +0200

    product page
    full product page -collapsible

--- product-app/Products.js
+++ product-app/Products.js
@@ -9,6 +9,7 @@ $(document).on('pagebeforeshow', '#home', function () {
 function renderCategory(results) {
     //this is the callBackFunc 
     results = $.parseJSON(results.d);
+    $('#DynamicList').empty();
     $.each(results, function (i, row) {
         dynamicLi = '<li> <a href="" data-id="' + row.Id + '"> <h3>' + row.Name + '</h3><span class="ui-li-count"> ' + row.ProductAmount+ '</span></a></li>'
         $('#DynamicList').append(dynamicLi);
@@ -25,18 +26,15 @@ $(document).on('vclick', '#DynamicList li a', function () {
         transition: "slide", changeHash: false
 
     });
-    //var CategoryInfo = {
-    //    id: null,
-    //    result:null
-    //}
+
 });
 
 function renderProducts(results) {
     //this is the callBackFunc 
     results = $.parseJSON(results.d);
-   // imageFullPath = ;
+    $('#DynamicListproducts').empty();
     $.each(results, function (i, row) {
-       console.log(JSON.stringify(row));
+      // console.log(JSON.stringify(row));
        if (row.ImagePath == null) { imageFullPath = "/images/no-img.jpg" }
             else { imageFullPath = row.ImagePath }
         dynamicList = '<li> <img src="'+ imageFullPath + '"/> <a href="" data-id="' + row.Id + '">  <h3>' + row.Title + '</h3><p>Price: ' + row.Price + '</br>Inventory: '+ row.Inventory + '</p></a></li>'
@@ -46,11 +44,36 @@ function renderProducts(results) {
 }
 
 
+$(document).on('vclick', '#DynamicListproducts li a', function () {
+    ProductInfo.id = $(this).attr('data-id');
+    getProduct(ProductInfo, renderFullProduct);
+    $.mobile.changePage("#product", {
+        transition: "slide", changeHash: false
+
+    });
+
+});
+
 function renderFullProduct(results) {
     //this is the callBackFunc 
     results = $.parseJSON(results.d);
-
-  
+    var attributes = "";
+    $('#productPage').empty();
+        if (results.ImagePath == null) { imageFullPath = "/images/no-img.jpg" }
+        else { imageFullPath = results.ImagePath }
+        pimginv = "<div style='text-align:center; padding-top:1em;padding-bottom:1em;'><h3>" + results.Title + "</h3></br><img src='" + imageFullPath + "'/> </br><p>Inventory: " + results.Inventory + "</p></div>"
+        $('#productPage').append(pimginv);
+        $.each(results.Attributes, function (i, row) {
+            if (results.Attributes == null) {
+                attributes = "<h4>No attributes for this product!</h4>"
+            }
+            else {
+                attributes = "<h4>" + i + "</h4>" +"<p>" + row + "</p>"
+            }
+        });
+        $('#productPageList').append(attributes);
+   
+    
 
 }
 
